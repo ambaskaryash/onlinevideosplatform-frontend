2eb58ae1dcfa5be81c0400fd3850e4c9e1cd536e
ambaskaryash <kimetsu119@gmail.com>
2025-01-27 20:36:06 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 
-export const server = "http://3.110.83.141:5001";
+export const server = "https://exercise.theflexingphysio.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
