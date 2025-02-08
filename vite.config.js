aac97fa3b68ce7f660047ea7af7bbe8d8c032a2a
ambaskaryash <kimetsu119@gmail.com>
2025-02-08 13:22:02 +0530

    vite config updates

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  base: '/',
+  server: {
     historyApiFallback: true,
   },
 })
